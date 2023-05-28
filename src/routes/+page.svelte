c5fcd54ab1215b2c92e8876b4fb2f07ca21bc7bb
wechu07 <wechu017@gmail.com>
2023-05-28 22:34:17 +0300

    update price logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,113 +1,117 @@
 <script>
-	import { onMount } from 'svelte';
-  
+	import { onMount, afterUpdate } from 'svelte';
+
 	let name = '';
 	let phone = '';
 	let classification = '';
 	let price = '';
-  
+
 	const classifications = [
-	  { value: '', label: 'Select Classification' },
-	  { value: 'test_subject', label: 'Test Subject' },
-	  { value: 'rotaractor', label: 'Rotaractor' },
-	  { value: 'rotarian', label: 'Rotarian' },
+		{ value: '', label: 'Select Classification' },
+		{ value: 'test', label: 'Test' },
+		{ value: 'rotaractor', label: 'Rotaractor' },
+		{ value: 'rotarian', label: 'Rotarian' }
 	];
-  
+
 	function handleSubmit() {
-	  // Perform API call and payment processing here
-	  // Display payment status message upon completion
-	  const paymentStatus = document.getElementById('paymentStatus');
-	  paymentStatus.innerText = 'Payment Complete!';
+		// Perform API call and payment processing here
+		// Display payment status message upon completion
+		const paymentStatus = document.getElementById('paymentStatus');
+		paymentStatus.innerText = 'Payment Complete!';
 	}
-  
-	// Update price based on the selected classification
+
 	function updatePrice() {
-	  switch (classification) {
-		case 'test_subject':
-		  price = 'KES 1';
-		  break;
-		case 'rotaractor':
-		  price = 'KES 1500';
-		  break;
-		case 'rotarian':
-		  price = 'KES 2000';
-		  break;
-		default:
-		  price = '';
-		  break;
-	  }
+		switch (classification) {
+			case 'test':
+				price = 'KES 1';
+				break;
+			case 'rotaractor':
+				price = 'KES 1500';
+				break;
+			case 'rotarian':
+				price = 'KES 2000';
+				break;
+			default:
+				price = '';
+				break;
+		}
 	}
-  
-	onMount(updatePrice);
-  </script>
-  
-  <main>
+
+	onMount(() => {
+		updatePrice();
+	});
+
+	afterUpdate(() => {
+		updatePrice();
+	});
+</script>
+
+<main>
 	<form on:submit={handleSubmit}>
-	  <label for="name">Name</label>
-	  <input type="text" id="name" bind:value={name} required>
-  
-	  <label for="phone">Phone Number</label>
-	  <input type="tel" id="phone" bind:value={phone} required>
-  
-	  <label for="classification">Classification</label>
-	  <select id="classification" bind:value={classification} required>
-		{#each classifications as { value, label }}
-		  <option value={value}>{label}</option>
-		{/each}
-	  </select>
-  
-	  <label for="price">Price</label>
-	  <input type="text" id="price" bind:value={price} readonly>
-  
-	  <button type="submit">Submit</button>
+		<label for="name">Name</label>
+		<input type="text" id="name" bind:value={name} required />
+
+		<label for="phone">Phone Number</label>
+		<input type="tel" id="phone" bind:value={phone} required />
+
+		<label for="classification">Classification</label>
+		<select id="classification" bind:value={classification} required>
+			{#each classifications as { value, label }}
+				<option {value}>{label}</option>
+			{/each}
+		</select>
+
+		<label for="price">Price</label>
+		<input type="text" id="price" bind:value={price} readonly />
+
+		<button type="submit">Submit</button>
 	</form>
-	<div id="paymentStatus"></div>
-  </main>
-  
-  <style>
+	<div id="paymentStatus" />
+</main>
+
+<style>
 	main {
-	  display: flex;
-	  flex-direction: column;
-	  align-items: center;
-	  margin-top: 20px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 20px;
 	}
-  
+
 	form {
-	  display: flex;
-	  flex-direction: column;
-	  align-items: center;
-	  width: 300px;
-	  padding: 20px;
-	  background-color: #fff;
-	  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 300px;
+		padding: 20px;
+		background-color: #fff;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 	}
-  
+
 	form label {
-	  font-weight: bold;
-	  margin-bottom: 5px;
+		font-weight: bold;
+		margin-bottom: 5px;
 	}
-  
+
 	form input,
 	form select {
-	  width: 100%;
-	  padding: 10px;
-	  margin-bottom: 10px;
-	  border: 1px solid #ccc;
-	  border-radius: 4px;
+		width: 100%;
+		padding: 10px;
+		margin-bottom: 10px;
+		border: 1px solid #ccc;
+		border-radius: 4px;
 	}
-  
+
 	form button {
-	  padding: 10px 20px;
-	  background-color: #4caf50;
-	  color: white;
-	  border: none;
-	  border-radius: 4px;
-	  cursor: pointer;
+		padding: 10px 20px;
+		background-color: #4caf50;
+		color: white;
+		border: none;
+		border-radius: 4px;
+		cursor: pointer;
 	}
-  
+
 	#paymentStatus {
-	  margin-top: 20px;
-	  text-align: center;
+		margin-top: 20px;
+		text-align: center;
 	}
-  </style>
-  
+</style>
